c1f68150e3b225b2cb31477de4623c6bebddeaf2
MANGO2412 <manuelgom3z22052003@gmail.com>
2023-03-01 12:27:31 -0800

    the third commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
- app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,'/public')));
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine','ejs');
+
 
 app.get('/',(req,res)=>{
-   res.sendFile(path.join(__dirname,'/public/index.html'))
+   res.render('index')
 })
 
 app.get('/about',(req,res)=>{
