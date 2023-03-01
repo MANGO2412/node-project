55c8fb9cc0df6b2c344a222624d66269b81740aa
MANGO2412 <manuelgom3z22052003@gmail.com>
2023-03-01 12:15:10 -0800

    first comit

---
+++ index.js
@@ -0,0 +1,5 @@
+const app=require('./src/app.js');
+const port =3000;
+app.listen(port,()=>{
+    console.log("the server web is running on port"+port);
+})
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+
+ app.use(express.static(path.join(__dirname,'/public')));
+
+app.get('/',(req,res)=>{
+   res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/about.html'))
+})
+
+module.exports=app;
